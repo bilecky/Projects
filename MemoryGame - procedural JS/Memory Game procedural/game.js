654cbec41a998ddb69f4348497be1d65b2240ae1
bilecky <51762310+bilecky@users.noreply.github.com>
2021-04-29 17:39:41 +0200

    test commit

---
+++ MemoryGame - procedural JS/Memory Game procedural/game.js
@@ -0,0 +1,9 @@
+const cardColors = ["red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "violet", "violet", "lightgreen", "lightgreen"];
+jhkg
+let cards = document.querySelectorAll("div")
+
+cards = [...cards]
+
+const init = function() {
+    
+}
