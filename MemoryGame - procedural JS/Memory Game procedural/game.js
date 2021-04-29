5389e4d796bac2b31cbb746f9f6d9252ec57bf0a
bilecky <51762310+bilecky@users.noreply.github.com>
2021-04-29 19:11:16 +0200

    finished, working Memory game

--- MemoryGame - procedural JS/Memory Game procedural/game.js
+++ MemoryGame - procedural JS/Memory Game procedural/game.js
@@ -1,9 +1,85 @@
-const cardColors = ["red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "violet", "violet", "lightgreen", "lightgreen"];
-jhkg
-let cards = document.querySelectorAll("div")
+const cardsColor = [
+  "red",
+  "red",
+  "green",
+  "green",
+  "blue",
+  "blue",
+  "brown",
+  "brown",
+  "yellow",
+  "yellow",
+  "gray",
+  "gray",
+  "cadetblue",
+  "cadetblue",
+  "violet",
+  "violet",
+  "lightgreen",
+  "lightgreen",
+];
 
-cards = [...cards]
+let cards = document.querySelectorAll("div");
 
-const init = function() {
-    
-}
+cards = [...cards];
+
+const startTime = new Date().getTime();
+
+let activeCard = "";
+const activeCards = [];
+const gamePairs = cards.length / 2;
+let gameResult = 0;
+
+const clickCard = function () {
+  let activeCard = this;
+  if (activeCard == activeCards[0]) {
+    return;
+  }
+  activeCard.classList.remove("hidden");
+
+  if (activeCards.length === 0) {
+    activeCards[0] = this;
+    return;
+  } else {
+    cards.forEach((card) => card.removeEventListener("click", clickCard));
+
+    activeCards[1] = activeCard;
+
+    setTimeout(function () {
+      if (activeCards[0].className === activeCards[1].className) {
+        activeCards.forEach((card) => card.classList.add("off"));
+        gameResult++;
+        cards = cards.filter((card) => !card.classList.contains("off"));
+
+        if (gameResult == gamePairs) {
+          const endTime = new Date().getTime();
+          const gameTime = (endTime - startTime) / 1000;
+          alert(`Wygrałeś! Czas gry to ${gameTime}s`);
+          location.reload();
+        }
+      } else {
+        activeCards.forEach((card) => card.classList.add("hidden"));
+      }
+
+      activeCard = "";
+      activeCards.length = 0;
+      cards.forEach((card) => card.addEventListener("click", clickCard));
+    }, 1000);
+  }
+};
+const init = function () {
+  cards.forEach(function (card) {
+    const position = Math.floor(Math.random() * cardsColor.length);
+    card.classList.add(cardsColor[position]);
+    cardsColor.splice(position, 1);
+  });
+
+  setTimeout(() => {
+    cards.forEach((card) => {
+      card.classList.add("hidden");
+      card.addEventListener("click", clickCard);
+    });
+  }, 2000);
+};
+
+init();
