818494e20d90c9dc7e42ad473c2b8af320e4980b
Pawel <bilecky95@gmail.com>
2021-12-04 00:59:10 +0100

    open & close EDIT popup done

--- ToDo Lista/script.js
+++ ToDo Lista/script.js
@@ -4,6 +4,13 @@ let addBtn // add task to list btn
 let ulList // task list
 let newTodo // new task
 
+let popup
+let popupInfo //popup text
+let todoToEdit // edit todo
+let popupInput // popup input
+let PopupAddBtn // add popup button
+let popupCloseBtn // cancel btn
+
 const main = () => {
 	prepareDOMElements()
 	prepareDOMEvents()
@@ -13,12 +20,20 @@ const prepareDOMElements = () => {
 	todoINput = document.querySelector('.todo-input')
 	errorInfo = document.querySelector('.error-info')
 	addBtn = document.querySelector('.btn-add')
-
 	ulList = document.querySelector('.todolist ul')
+
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	PopupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
 }
+
 const prepareDOMEvents = () => {
 	//event listeners
 	addBtn.addEventListener('click', addNewTodo)
+	ulList.addEventListener('click', checkCLick)
+	popupCloseBtn.addEventListener('click', closePopup)
 }
 
 //1. make  nee LI
@@ -32,7 +47,7 @@ const addNewTodo = () => {
 	if (todoINput.value !== '') {
 		newTodo = document.createElement('li')
 		newTodo.textContent = todoINput.value
-        createTOolsArea()
+		createTOolsArea()
 
 		ulList.append(newTodo)
 		todoINput.value = ''
@@ -47,8 +62,7 @@ const addNewTodo = () => {
 const createTOolsArea = () => {
 	const toolsPanel = document.createElement('div')
 	toolsPanel.classList.add('tools')
-    newTodo.append(toolsPanel)
-
+	newTodo.append(toolsPanel)
 
 	const completeBtn = document.createElement('button')
 	completeBtn.classList.add('complete')
@@ -63,6 +77,24 @@ const createTOolsArea = () => {
 	deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
 
 	toolsPanel.append(completeBtn, editBtn, deleteBtn)
+}
+
+const checkCLick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.classList.toggle('completed')
+		e.target.closest('li').classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		editTodo()
+	} else if (e.target.matches('.delete')) {
+		console.log('delete')
+	}
+}
+
+const editTodo = () => {
+	popup.style.display = 'flex'
+}
+const closePopup = () => {
+	popup.style.display = 'none'
 }
 
 document.addEventListener('DOMContentLoaded', main)
